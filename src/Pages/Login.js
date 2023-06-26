802cb0054096bd0c72919508baf6e2ba332f5351
seema987 <seemasitaula987@email.com>
2023-06-27 09:05:16 +0930

    login and signup updated

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,15 +1,64 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { useNavigate } from "react-router-dom";
+
+
 
 const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleUsernameChange = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value)
+    }
+
+    
+const navigate = useNavigate()
+
+
+    const handleLogin = async () => {
+        
+        fetch('http://localhost:3001/api/sessions', {
+            method: 'POST',
+            headers: { 'Content-Type' : 'application/json' },
+            body: JSON.stringify({
+                email,
+                password
+            })
+        })
+        .then(res => res.json())
+        .then(res =>  {
+            //console.log('logged in successfully')
+            if(res.error) {
+               navigate('/login')
+              
+            } else {
+                
+                navigate('/')
+            }
+        })
+    }
+    
+    function handleError(errorMessage) {
+        document.querySelector('#page').innerHTML =  `
+        <p style='color: red;'>${errorMessage}</p>
+        ` + document.querySelector('#page').innerHTML
+    }
     return (
         <div className='login'>
             <h1>Login</h1>
-            <form action="">
-                <input type="text" placeholder='username' />
-                <input type="password" placeholder='password' />
-                <button>Login</button>
-               
+            <form>
+                <input type="text" placeholder='username' onChange={handleUsernameChange}/>
+                <input type="password" placeholder='password' onChange={handlePasswordChange}/>
+                
+                <button type="button" onClick={() => handleLogin()}> Login</button>
+                
+             
                 <span>Don't have an account? <Link to="/signup">Signup</Link></span>
             </form>
         </div>
--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,18 +1,51 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
+
 
 const Signup = () => {
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value)
+    }
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const handleSignup = async () => {
+
+        fetch('/api/users', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                username,
+                password,
+                email
+            })
+        })
+            .then(res => res.json())
+            .then(email => { })
+    }
+
     return (
         <div className='signup'>
             <h1>Signup</h1>
-            <form action="">
-                <input type="text" placeholder='username' />
-                <input type="email" placeholder='email' />
-                <input type="password" placeholder='password' />
-                <button>Sign Up</button>
-                
-                <span>Have an account? <Link to="/login">Login</Link></span>
+            <form>
+                <input type="text" placeholder='username' onChange={handleUsernameChange} />
+                <input type="email" placeholder='email' onChange={handleEmailChange} />
+                <input type="password" placeholder='password' onChange={handlePasswordChange} />
+                <button type="button" onClick={() => handleSignup()}>Sign Up</button>
             </form>
+            <span>Have an account? <Link to="/login">Login</Link></span>
         </div>
     )
 }
